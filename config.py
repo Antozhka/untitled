831c9b0b9fb23556be98789fff2d97b18757a598
Antozhka <agashturi@gmail.com>
2019-04-15 16:50:01 +0300

    2019.04.15 - закомментил Bootstrap - отлов блох

--- config.py
+++ config.py
@@ -5,4 +5,23 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you will never quess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
                               'sqlite:///' + os.path.join(basedir, 'app.db')
+    POSTS_PER_PAGE = 10
+    '''
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    '''
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = 'gasht.examples@gmail.com'
+    MAIL_PASSWORD = 'cfvsqkexibqgfhjkm'
+    MAIL_DEFAULT_SENDER = 'flask@example.com'
+    ADMINS = ['agashturi@gmail.com']
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    #CELERY_BROKER_URL = 'redis://localhost:6379/0'
+    #CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+    #CELERY_BROKER_URL = 'amqp://'
+    #CELERY_RESULT_BACKEND = 'amqp'
